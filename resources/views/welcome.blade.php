0a366fe969755678778b4422ea3f203174daef6c
Hardip Patel <hardip@anormaly.com>
2023-07-28 09:15:17 +0530

    #1: Simplify welcome page structure, add new footer component, modify guest layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,35 +1,14 @@
 <x-guest-layout>
 
-    <div
-        class="relative flex justify-center items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-        @if (false && Route::has('login'))
-            <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                @auth
-                    <a href="{{ url('/dashboard') }}"
-                       class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-                @else
-                    <a href="{{ route('login') }}"
-                       class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log
-                        in</a>
+    <div class="max-w-7xl mx-auto p-6 lg:p-8">
 
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}"
-                           class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                    @endif
-                @endauth
+        <div class="flex flex-col items-center space-y-4">
+            <x-application-logo class="w-16 h-16"/>
+
+            <div class="text-orange-900 dark:text-orange-300 font-black font-mono">
+                Laravel Community Vadodara
             </div>
-        @endif
-
-        <div class="max-w-7xl mx-auto p-6 lg:p-8">
-
-            <div class="flex flex-col items-center space-y-4">
-                <x-application-logo class="w-16 h-16"/>
-
-                <div class="text-orange-900 dark:text-orange-300 font-black font-mono">
-                    Laravel Community Vadodara
-                </div>
-            </div>
-
         </div>
+
     </div>
 </x-guest-layout>
